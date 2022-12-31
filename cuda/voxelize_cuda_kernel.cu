54a4aa8dbce272f76bcd890070072bf689463d97
Carlos Barreto <carlosedubarreto@gmail.com>
2022-12-31 06:44:19 -0300

    updated to compile with py3.8 and cuda 11.6

--- cuda/voxelize_cuda_kernel.cu
+++ cuda/voxelize_cuda_kernel.cu
@@ -11,16 +11,32 @@
 #define EPSILON_ABS_ZERO 1e-10
 #define EPSILON_DIV_ZERO 1e-4
 
-// for the older gpus atomicAdd with double arguments does not exist
-#if  __CUDA_ARCH__ < 600 and defined(__CUDA_ARCH__)
-static __inline__ __device__ double atomicAdd(double* address, double val) {
+// // for the older gpus atomicAdd with double arguments does not exist
+// #if  __CUDA_ARCH__ < 600 and defined(__CUDA_ARCH__)
+// static __inline__ __device__ double atomicAdd(double* address, double val) {
+//     unsigned long long int* address_as_ull = (unsigned long long int*)address;
+//     unsigned long long int old = *address_as_ull, assumed;
+//     do {
+//         assumed = old;
+//         old = atomicCAS(address_as_ull, assumed,
+//                 __double_as_longlong(val + __longlong_as_double(assumed)));
+//     // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN) } while (assumed != old);
+//     } while (assumed != old);
+//     return __longlong_as_double(old);
+// }
+// #endif
+
+#if !defined(CUDA_ARCH) || CUDA_ARCH >= 600
+
+#else
+static inline __device__ double atomicAdd(double* address, double val) {
     unsigned long long int* address_as_ull = (unsigned long long int*)address;
     unsigned long long int old = *address_as_ull, assumed;
+    if (val==0.0)
+    return __longlong_as_double(old);
     do {
         assumed = old;
-        old = atomicCAS(address_as_ull, assumed,
-                __double_as_longlong(val + __longlong_as_double(assumed)));
-    // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN) } while (assumed != old);
+        old = atomicCAS(address_as_ull, assumed, __double_as_longlong(val +__longlong_as_double(assumed)));
     } while (assumed != old);
     return __longlong_as_double(old);
 }
